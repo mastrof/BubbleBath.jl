3b896bb68dcb3c162020a966a98e8ba402654612
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-24 15:07:01 +0100

    add return types

--- src/packing_fraction.jl
+++ src/packing_fraction.jl
@@ -7,7 +7,7 @@ Evaluate the packing fraction of `spheres` in domain `extent`.
 function packing_fraction(
     spheres::Vector{Sphere{D}},
     extent::NTuple{D,Float64}
-) where D
+)::Float64 where D
     V₀ = prod(extent)
     V = 0.0
     for sphere in spheres
@@ -20,12 +20,12 @@ end
     volume(sphere::Sphere{2})
 Evaluate volume of two-dimensional sphere, i.e. the area of a circle (πr²).
 """
-volume(sphere::Sphere{2}) = π * sphere.radius^2
+volume(sphere::Sphere{2})::Float64 = π * sphere.radius^2
 """
     volume(sphere::Sphere{3})
 Evaluate volume of a three-dimensional sphere (4πr³/3)
 """
-volume(sphere::Sphere{3}) = 4π/3 * sphere.radius^3
+volume(sphere::Sphere{3})::Float64 = 4π/3 * sphere.radius^3
 
 """
     packing_fraction(radii::Vector{Real}, extent::NTuple{D,Float64}) where D
@@ -35,7 +35,7 @@ in domain `extent`.
 function packing_fraction(
     radii::Vector{Real},
     extent::NTuple{D, Float64}
-) where D
+)::Float64 where D
     V₀ = prod(extent)
     V = 0.0
     for radius in radii
@@ -49,7 +49,7 @@ end
 Evaluate volume of a sphere of radius `r` in `D` dimensions.
 Only D=2 and D=3 currently supported.
 """
-function volume(r::Real, D::Int)
+function volume(r::Real, D::Int)::Float64
     if D == 2
         return π*r^2
     elseif D == 3
