2ba7cddc39b75659c7791c79291084a323e353a6
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-31 00:12:46 +0100

    make `Sphere` more robust by using inner constructor

--- src/BubbleBath.jl
+++ src/BubbleBath.jl
@@ -4,19 +4,19 @@ using LinearAlgebra: norm
 
 export Sphere
 
-struct Sphere{D}
-    pos::NTuple{D,Float64}
-    radius::Float64
-end
 """
     Sphere(pos::NTuple{D,Real}, radius::Real) where D
 Create a `Sphere{D}` object centered at `pos` with radius `radius`.
 """
-function Sphere(pos::NTuple{D,Real}, radius::Real) where D
-    if radius ≤ 0
-        throw(ArgumentError("Sphere radius must be a non-negative real number."))
+struct Sphere{D}
+    pos::NTuple{D,Float64}
+    radius::Float64
+    function Sphere(pos::NTuple{D,Real}, radius::Real) where D
+        if radius ≤ 0
+            throw(ArgumentError("Sphere radius must be a non-negative real number."))
+        end
+        new{D}(Float64.(pos), Float64(radius))
     end
-    Sphere{D}(Float64.(pos),Float64(radius))
 end
 
 include("bubblebath_algorithm.jl")
