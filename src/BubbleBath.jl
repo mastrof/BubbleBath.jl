3e417553402fdb953abe24556c7b7459dbbe5e0a
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-30 19:47:53 +0100

    throw error for negative sphere radius

--- src/BubbleBath.jl
+++ src/BubbleBath.jl
@@ -12,7 +12,12 @@ end
     Sphere(pos::NTuple{D,Real}, radius::Real) where D
 Create a `Sphere{D}` object centered at `pos` with radius `radius`.
 """
-Sphere(pos::NTuple{D,Real}, radius::Real) where D = Sphere{D}(Float64.(pos),Float64(radius))
+function Sphere(pos::NTuple{D,Real}, radius::Real) where D
+    if radius ≤ 0
+        throw(ArgumentError("Sphere radius must be a non-negative real number."))
+    end
+    Sphere{D}(Float64.(pos),Float64(radius))
+end
 
 include("bubblebath_algorithm.jl")
 include("packing_fraction.jl")
