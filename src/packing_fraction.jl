37a1b88d66d4d293eca7add0b7352cd037c15873
Riccardo Foffi <riccardo.foffi@protonmail.com>
2022-12-31 13:18:49 +0100

    update `packing_fraction` docstring

--- src/packing_fraction.jl
+++ src/packing_fraction.jl
@@ -64,6 +64,12 @@ end
 """
     packing_fraction(wm::BitArray)
 Evaluate the packing fraction in a walkmap `wm`.
+
+If `wm` was generated with `probe_radius=0` this represents
+the real packing fraction, instead if `probe_radius` was not
+zero, it represents the effective packing fraction experienced
+by the probe.
+
 Unlike the other instances of `packing_fraction`, this one
 is exact, independently of overlaps and boundary conditions,
 within the resolution of the walkmap.
